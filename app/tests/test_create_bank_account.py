2a4e7ed6309aad6d0c1c463a64aa2444a2dfcfd3
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2022-10-21 15:14:24 +0200

    dodanie peselu do konta

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,9 +5,10 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "60102100354")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.pesel, "60102100354", "Pesel nie został zapisany!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
 
     #tutaj proszę dodawać nowe testy
