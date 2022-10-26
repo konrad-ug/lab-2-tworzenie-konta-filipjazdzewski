ccc480660fbfce66dc6ac38e93fb7f91dfb843b8
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2022-10-26 19:54:06 +0200

    feature 3: pesel validation

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,9 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, pesel, imie, nazwisko):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.pesel = pesel
         self.saldo = 0
+        self.checkPeselValidation(pesel)
+        
+    def checkPeselValidation(self, pesel):
+        self.pesel = pesel if len(pesel) == 11 else 'Niepoprawny pesel!'
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -3,12 +3,21 @@ import unittest
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
+    correct_pesel = "99111900536"
+    name = "Dariusz"
+    surname = "Januszewski"
+
+    incorrect_pesel = "12345"
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski", "60102100354")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.pesel, "60102100354", "Pesel nie został zapisany!")
+        pierwsze_konto = Konto(self.correct_pesel, self.name, self.surname)
+        self.assertEqual(pierwsze_konto.pesel, self.correct_pesel, "Pesel nie został zapisany!")
+        self.assertEqual(pierwsze_konto.imie, self.name, "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, self.surname, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-
-    #tutaj proszę dodawać nowe testy
+        
+    def test_pesel_lenght_is_not_correct(self):
+        drugie_konto = Konto(self.incorrect_pesel, self.name, self.surname)
+        self.assertEqual(drugie_konto.pesel, "Niepoprawny pesel!",
+            "Pesel nie posiada wartości 'Niepoprawny pesel', przy podaniu niepoprawnego peselu!")
+            
