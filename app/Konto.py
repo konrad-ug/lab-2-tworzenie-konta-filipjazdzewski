117a07955d2b129baf720cdef9e573537c8c1b1e
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2022-10-28 14:26:07 +0200

    poprawienie nazw funkcji: usuniecie 'check'

--- app/Konto.py
+++ app/Konto.py
@@ -4,13 +4,13 @@ class Konto:
     def __init__(self, pesel, imie, nazwisko, coupon=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.checkPeselValidation(pesel)
-        self.checkCouponValidation(coupon)
+        self.PeselValidation(pesel)
+        self.CouponValidation(coupon)
         
-    def checkPeselValidation(self, pesel):
+    def PeselValidation(self, pesel):
         self.pesel = pesel if len(pesel) == 11 else 'Niepoprawny pesel!'
 
-    def checkCouponValidation(self, coupon):
+    def CouponValidation(self, coupon):
         isCouponValid = coupon != None and coupon.startswith("PROM_") and len(coupon) == 8
         isUserYoungEnough = self.pesel != "Niepoprawny pesel!" and (int(self.pesel[0:2]) > 60 or int(self.pesel[2:4]) > 20)
         if (isCouponValid and isUserYoungEnough):
