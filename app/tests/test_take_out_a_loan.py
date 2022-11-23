26bffca70e24f84004a0568229b2f1f5deb46765
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2022-11-23 17:33:55 +0100

    added setUp

--- app/tests/test_take_out_a_loan.py
+++ app/tests/test_take_out_a_loan.py
@@ -7,31 +7,29 @@ class TestTakingOutALoan(unittest.TestCase):
     surname = "Januszewski"
     pesel = "99111900536"
 
+    def setUp(self):
+        self.account = Account(self.pesel, self.surname, self.name)
+
     def test_5_incoming_transfers(self):
-        account = Account(self.pesel, self.surname, self.name)
-        account.history = [-100, 100, 100, 100, 600]
-        is_granted = account.TakeOutALoan(500)
+        self.account.history = [-100, 100, 100, 100, 600]
+        is_granted = self.account.TakeOutALoan(500)
         self.assertTrue(is_granted)
-        self.assertEqual(account.balance, 500)
+        self.assertEqual(self.account.balance, 500)
 
     def test_5_incoming_transfers_too_little_sum(self):
-        account = Account(self.pesel, self.surname, self.name)
-        account.history = [-10000, 100, 100, 100, 600]
-        is_granted = account.TakeOutALoan(500)
+        self.account.history = [-10000, 100, 100, 100, 600]
+        is_granted = self.account.TakeOutALoan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
+        self.assertEqual(self.account.balance, 0)
 
     def test_last_3_transfers_not_all_positive(self):
-        account = Account(self.pesel, self.surname, self.name)
-        account.history = [10000, -100, 0, 100, 600]
-        is_granted = account.TakeOutALoan(500)
+        self.account.history = [10000, -100, 0, 100, 600]
+        is_granted = self.account.TakeOutALoan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
+        self.assertEqual(self.account.balance, 0)
 
     def test_history_length_below_5(self):
-        account = Account(self.pesel, self.surname, self.name)
-        account.history = [100, 300, 100, 600]
-        is_granted = account.TakeOutALoan(500)
+        self.account.history = [100, 300, 100, 600]
+        is_granted = self.account.TakeOutALoan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
-        
+        self.assertEqual(self.account.balance, 0)
